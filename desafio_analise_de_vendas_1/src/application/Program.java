baae8092d263c42eeb29915edf44f2188d89f874
Matheus Granville <matheusgranville@gmail.com>
2025-06-29 23:27:59 -0300

    Desafio Analise de Vendas 1 - modulo 17

---
+++ desafio_analise_de_vendas_1/src/application/Program.java
@@ -0,0 +1,59 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Sale;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Entre o caminho do arquivo: ");
+		String path = sc.nextLine();
+		List<Sale> sales = new ArrayList<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+
+			while (line != null) {
+				String[] fields = line.split(",");
+				Integer month = Integer.parseInt(fields[0]);
+				Integer year = Integer.parseInt(fields[1]);
+				String seller = fields[2];
+				Integer items = Integer.parseInt(fields[3]);
+				Double total = Double.parseDouble(fields[4]);
+				sales.add(new Sale(month, year, seller, items, total));
+				line = br.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Erro: " + path + " (O sistema não pode encontrar o arquivo especificado)");
+		} catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+
+		System.out.println();
+		System.out.println("Cinco primeiras vendas de 2016 de maior preço médio");
+		sales.stream().filter(sale -> sale.getYear() == 2016)
+				.sorted((x, y) -> y.averagePrice().compareTo(x.averagePrice())).limit(5).forEach(System.out::println);
+
+		Double loganTotalValue = sales.stream()
+				.filter(sale -> sale.getSeller().equals("Logan") && (sale.getMonth() == 1 || sale.getMonth() == 7))
+				.map(sale -> sale.getTotal()).reduce(0.0, (x, y) -> x + y);
+
+		System.out.println();
+		System.out.printf("Valor total vendido pelo vendedor Logan nos meses 1 e 7 = %.2f", loganTotalValue);
+
+		sc.close();
+	}
+
+}
